498f53aa3766d084f117de2c6629775e1310bba8
vini6161610 <141057880+vini6161610@users.noreply.github.com>
2024-09-23 16:09:40 -0300

    ddd

--- formulario/recebe.php
+++ formulario/recebe.php
@@ -4,14 +4,12 @@ if(isset($_POST["nome"]) or isset($_POST["email"]))  {
     
     $nome = $_POST["nome"];
     $email = $_POST["email"];
-    $tele = $_POST["tele"];
-    $endereco = $_POST["endereco"];
+ 
 
     echo "<h2>Dados recebidos com sucesso!</h2>";
     echo "Usuario: $nome <br>";
     echo "E-mail: $email; <br>";
-    echo "Telefone: $tele <br>";
-    echo "Endereço: $endereco";
+  
     
 
 }
